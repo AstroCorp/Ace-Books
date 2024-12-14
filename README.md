a44d75d3e34daa3aebc3ea7effcf4af31ad55329
Alejandro Leal <aleaberm12@gmail.com>
2024-12-15 00:12:01 +0100

    docker

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Ace Books
 
 Repositorio para trabajar con el frontend y el backend clonando un solo proyecto.
+
+## Instrucciones
+
+1. Ejecutar el script `setup.sh` para generar el .env de ambos repositorios junto a sus enlaces simbólicos
+2. Ejecutar `docker-compose build` para generar las imágenes de docker
+3. Levantar el proyecto con `docker-compose up`
+4. Para detener el proyecto usar `docker-compose down`
---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ENV_FILE="./.env"
+FRONTEND_DIR="./Ace-Books-Frontend"
+BACKEND_DIR="./Ace-Books-Backend"
+
+# Comprobar si el archivo .env existe
+if [ -f "$ENV_FILE" ]; then
+    read -p "The .env file already exists. Do you want to replace it? (y/N): " REPLACE
+
+    if [ "$REPLACE" != "y" ]; then
+        echo "The .env file will not be replaced."
+    else
+        # Crear el archivo .env combinando los .env.example de frontend y backend
+        {
+            echo "# --- Frontend ---"
+            echo ""
+            cat "$FRONTEND_DIR/.env.example"
+            echo ""
+            echo "# --- Backend ---"
+            echo ""
+            cat "$BACKEND_DIR/.env.example"
+        } > "$ENV_FILE"
+
+        echo ".env file generated with the content from the frontend and backend .env.example files"
+    fi
+fi
+
+# Crear enlaces simbólicos en el frontend y en el backend para gestionar el .env
+if [[ "$OSTYPE" == "msys" ]]; then
+    cmd //c "cd %CD%\\Ace-Books-Frontend && mklink /H .env ..\.env"
+else
+    cd ./Ace-Books-Frontend && ln -s "../.env" ".env"
+fi
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    cmd //c "cd %CD%\\Ace-Books-Backend && mklink /H .env ..\.env"
+else
+    cd ../Ace-Books-Backend && ln -s "../.env" ".env"
+fi
+
+echo "Symbolic links created in $FRONTEND_DIR and $BACKEND_DIR"

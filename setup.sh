aee700179d3322e2384d7f4be7492656b1c08159
Alejandro Leal <aleaberm12@gmail.com>
2025-04-07 19:38:44 +0200

    refactor(setup): Mejora en la generación del archivo .env y manejo de enlaces simbólicos

--- setup.sh
+++ setup.sh
@@ -10,9 +10,25 @@ ENV_FILE="./.env"
 FRONTEND_DIR="./Ace-Books-Frontend"
 BACKEND_DIR="./Ace-Books-Backend"
 
+generate_env_file() {
+    # Crear el archivo .env combinando los .env.example de frontend y backend
+    {
+        echo "# --- Frontend ---"
+        echo ""
+        cat "$FRONTEND_DIR/.env.example"
+        echo ""
+        echo "# --- Backend ---"
+        echo ""
+        cat "$BACKEND_DIR/.env.example"
+    } > "$ENV_FILE"
+
+    echo -e "${COLOR_GREEN}.env file generated with the content from the frontend and backend .env.example files.${COLOR_DEFAULT}"
+}
+
 # Si no existe el directorio del frontend, clonar el repositorio
 if [ ! -d "$FRONTEND_DIR" ]; then
     git clone git@github.com:AstroCorp/Ace-Books-Frontend.git
+    
     echo -e "${COLOR_GREEN}Frontend repository cloned.${COLOR_DEFAULT}"
 else
     echo -e "${COLOR_YELLOW}The frontend directory already exists.${COLOR_DEFAULT}"
@@ -21,6 +37,7 @@ fi
 # Si no existe el directorio del backend, clonar el repositorio
 if [ ! -d "$BACKEND_DIR" ]; then
     git clone git@github.com:AstroCorp/Ace-Books-Backend.git
+
     echo -e "${COLOR_GREEN}Backend repository cloned.${COLOR_DEFAULT}"
 else
     echo -e "${COLOR_YELLOW}The backend directory already exists.${COLOR_DEFAULT}"
@@ -29,6 +46,7 @@ fi
 # Comprobar si el archivo .env existe
 if [ -f "$ENV_FILE" ]; then
     echo -e "${COLOR_BLUE}The .env file already exists. Do you want to replace it? (y/N):${COLOR_DEFAULT}"
+
     read -p "" REPLACE
 
     if [ "$REPLACE" != "y" ]; then
@@ -37,39 +55,44 @@ if [ -f "$ENV_FILE" ]; then
         # Eliminar el archivo .env
         rm "$ENV_FILE"
 
-        # Crear el archivo .env combinando los .env.example de frontend y backend
-        {
-            echo "# --- Frontend ---"
-            echo ""
-            cat "$FRONTEND_DIR/.env.example"
-            echo ""
-            echo "# --- Backend ---"
-            echo ""
-            cat "$BACKEND_DIR/.env.example"
-        } > "$ENV_FILE"
-
-        echo -e "${COLOR_GREEN}.env file generated with the content from the frontend and backend .env.example files.${COLOR_DEFAULT}"
+        generate_env_file
     fi
+else
+    generate_env_file
 fi
 
 # Crear enlaces simbólicos en el frontend y en el backend para gestionar el .env
 if [ ! -f "$FRONTEND_DIR/.env" ]; then
+    # Eliminamos el .env antes de crear el enlace simbólico
+    if [ -f "$FRONTEND_DIR/.env" ]; then
+        rm "$FRONTEND_DIR/.env"
+    fi
+
+    # Creamos el enlace simbólico tenido en cuenta el sistema operativo
     if [[ "$OSTYPE" == "msys" ]]; then
         cmd //c "cd %CD%\\Ace-Books-Frontend && mklink /H .env ..\.env"
     else
         cd ./Ace-Books-Frontend && ln -s "../.env" ".env"
     fi
+
     echo -e "${COLOR_GREEN}Symbolic link created in $FRONTEND_DIR${COLOR_DEFAULT}"
 else
     echo -e "${COLOR_YELLOW}The symbolic link in $FRONTEND_DIR already exists.${COLOR_DEFAULT}"
 fi
 
 if [ ! -f "$BACKEND_DIR/.env" ]; then
+    # Eliminamos el .env antes de crear el enlace simbólico
+    if [ -f "$BACKEND_DIR/.env" ]; then
+        rm "$BACKEND_DIR/.env"
+    fi
+
+    # Creamos el enlace simbólico tenido en cuenta el sistema operativo
     if [[ "$OSTYPE" == "msys" ]]; then
         cmd //c "cd %CD%\\Ace-Books-Backend && mklink /H .env ..\.env"
     else
         cd ../Ace-Books-Backend && ln -s "../.env" ".env"
     fi
+
     echo -e "${COLOR_GREEN}Symbolic link created in $BACKEND_DIR${COLOR_DEFAULT}"
 else
     echo -e "${COLOR_YELLOW}The symbolic link in $BACKEND_DIR already exists.${COLOR_DEFAULT}"
